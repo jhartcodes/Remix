a123d89599092b3c3c6ae742552b9505edeaccb1
Joel <joel.d.hart1@gmail.com>
2023-11-04 14:51:03 -0700

    got markdown

--- exercise/03-dynamic-params/app/routes/posts/$slug.tsx
+++ exercise/03-dynamic-params/app/routes/posts/$slug.tsx
@@ -2,21 +2,26 @@ import type { LoaderArgs } from "@remix-run/node";
 import { json } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
 import { getPost } from "~/models/post.server";
+import { marked } from "marked";
 
-export async function loader({ request, params }: LoaderArgs) {
+export async function loader({ params }: LoaderArgs) {
   const post = await getPost(params.slug);
+  const html = marked(post.markdown);
+  console.log(post, "post return");
   return json({
     post,
+    html,
   });
 }
 
 export default function PostSlug() {
-  const data = useLoaderData<typeof loader>();
+  const { post, html } = useLoaderData<typeof loader>();
+
   return (
     <main className="mx-auto max-w-4xl">
-      <h1 className="my-6 border-b-2 text-center text-3xl">
-        {data.post.title}
-      </h1>
+      <h1 className="my-6 border-b-2 text-center text-3xl">{post?.title}</h1>
+
+      <div dangerouslySetInnerHTML={{ __html: html }} />
     </main>
   );
 }
